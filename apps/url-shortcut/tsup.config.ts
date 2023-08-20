fa6df2b3a861086a0c5d8ac2e24b92a601a33250
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-08-20 15:24:28 -0400

    chore: renovate config

--- apps/url-shortcut/tsup.config.ts
+++ apps/url-shortcut/tsup.config.ts
@@ -1,20 +1,53 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig(({ minify }) => {
-  const inProduction = !!minify;
+export default defineConfig(({ define }) => {
+  const buildMode = expr(() => {
+    if (define) {
+      if (hasProp(define, "mode")) {
+        return define.mode;
+      }
+    }
+    return "unknown";
+  });
 
-  console.log(`Building for ${inProduction ? "production" : "development"}...`);
+  console.log(`Building in ${buildMode} mode...\n\n`);
+
+  if (buildMode === "unknown") {
+    return { name: buildMode, dts: true, clean: true };
+  }
+
+  if (buildMode === "custom") {
+    return { name: buildMode, clean: true, dts: true };
+  }
+
+  const inProduction = buildMode === "production";
 
   return {
+    name: buildMode,
     dts: true,
     clean: true,
-    minify,
-    bundle: inProduction,
+    minify: inProduction,
     splitting: inProduction,
     treeshake: inProduction,
-    format: ["cjs", "esm"],
+    format: inProduction ? ["cjs", "esm"] : "esm",
     entry: {
       core: "src/index.ts",
     },
+    noExternal: ["@baton-pass/gql-canvas"],
   };
 });
+
+function expr<T>(lam: () => T): T {
+  return lam();
+}
+
+type HasProp<T extends string> = {
+  [P in T]: string;
+} & Record<string, string>;
+
+function hasProp<P extends string,>(
+  ob: Record<string, string>,
+  prop: P,
+): ob is HasProp<P> {
+  return prop in ob;
+}
