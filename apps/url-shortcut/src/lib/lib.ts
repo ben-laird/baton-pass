a44529f54a25b8c57431610a3d7cee6826567573
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-08-20 22:20:44 -0400

    refactor: add more things zod schemas

--- apps/url-shortcut/src/lib/lib.ts
+++ apps/url-shortcut/src/lib/lib.ts
@@ -2,13 +2,11 @@ import { z } from "zod";
 import { parseDate } from "chrono-node";
 import { format, parseISO } from "date-fns";
 
+// === UTILITY SCHEMAS ===
+
 // Things date strings
 
-export type DateStringInput = z.input<typeof dateStringInput>;
-
-export const dateStringInput = z.union([z.string(), z.date()]);
-
-export const dateString = dateStringInput.transform((a) => {
+const dateString = z.union([z.string(), z.date()]).transform((a) => {
   if (typeof a === "string") {
     if (a === "anytime" || a === "someday") {
       return a;
@@ -18,87 +16,177 @@ export const dateString = dateStringInput.transform((a) => {
   return format(a, "MMMM do, y");
 });
 
-export type DateString = z.output<typeof dateString>;
-
 // Things time strings
 
 const timeRegex =
   /((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))|^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 
-export type TimeStringInput = z.input<typeof timeStringInput>;
-
-export const timeStringInput = z.union([z.string().regex(timeRegex), z.date()]);
-
-export const timeString = timeStringInput.transform((a) => {
-  if (typeof a === "string") {
-    return format(parseDate(a), "h aaa");
-  }
-  return format(a, "h aaa");
-});
-
-export type TimeString = z.output<typeof timeString>;
+const timeString = z
+  .union([z.string().regex(timeRegex), z.date()])
+  .transform((a) => {
+    if (typeof a === "string") {
+      return format(parseDate(a), "h aaa");
+    }
+    return format(a, "h aaa");
+  });
 
 // Things datetime strings
 
-export type DateTimeStringInput = z.input<typeof dateStringInput>;
+export const dateTimeString = z
+  .union([z.string().datetime(), z.string().includes("@"), z.date()])
+  .transform((a, ctx) => {
+    if (typeof a === "string") {
+      const timeStringFilter = z.string().datetime().safeParse(a);
 
-export const dateTimeStringInput = z.union([
-  z.string().datetime(),
-  z.string().includes("@"),
-  z.date(),
+      if (!timeStringFilter.success) {
+        const dateTimeTuple = z
+          .tuple([dateString, timeString])
+          .safeParse(a.split("@"));
+
+        if (!dateTimeTuple.success) {
+          dateTimeTuple.error.issues.forEach((iss) => ctx.addIssue(iss));
+          return z.NEVER;
+        }
+        return dateTimeTuple.data.join("@");
+      }
+      return format(parseISO(a), "MMMM do, y@h aaa");
+    }
+    return format(a, "MMMM do, y@h aaa");
+  });
+
+// Other utilities
+
+const when = z
+  .union([
+    z.enum(["today", "tomorrow", "evening", "anytime", "someday"]),
+    dateString,
+  ])
+  .optional();
+
+// === FINAL SCHEMA ===
+
+// These two are exported temporarily
+
+export const headingSchema = z.object({
+  type: z.literal("heading"),
+  attributes: z.object({
+    title: z.string().optional(),
+    archived: z.boolean().optional(),
+  }),
+});
+
+export const checklistItemSchema = z.object({
+  type: z.literal("checklist-item"),
+  attributes: z.object({
+    title: z.string().optional(),
+    completed: z.boolean().optional(),
+    canceled: z.boolean().optional(),
+  }),
+});
+
+// Things To-Do
+
+const todoAttributes = z.object({
+  title: z.string().optional(),
+  notes: z.string().optional(),
+  when,
+  deadline: dateString.optional(),
+  tags: z.string().array().nonempty().optional(),
+  "checklist-items": z.string().array().nonempty().max(100).optional(),
+  "list-id": z.string().optional(),
+  list: z.string().optional(),
+  "heading-id": z.string().optional(),
+  heading: z.string().optional(),
+  completed: z.boolean().optional(),
+  canceled: z.boolean().optional(),
+  "creation-date": dateString.optional(),
+  "completion-date": dateString.optional(),
+});
+
+export const todoSchema = z.union([
+  z.object({
+    type: z.literal("to-do"),
+    operation: z.literal("create"),
+    attributes: todoAttributes,
+  }),
+  z.object({
+    type: z.literal("to-do"),
+    operation: z.literal("update"),
+    attributes: todoAttributes.extend({
+      "prepend-notes": z.string().optional(),
+      "append-notes": z.string().optional(),
+      "add-tags": z
+        .string()
+        .array()
+        .nonempty()
+        .transform((a) => {
+          return a.join(",");
+        })
+        .optional(),
+      "prepend-checklist-items": z
+        .string()
+        .array()
+        .nonempty()
+        .transform((a) => {
+          return a.join("\n");
+        })
+        .optional(),
+      "append-checklist-items": z
+        .string()
+        .array()
+        .nonempty()
+        .transform((a) => {
+          return a.join("\n");
+        })
+        .optional(),
+    }),
+  }),
 ]);
 
-export const dateTimeString = dateTimeStringInput.transform((a, ctx) => {
-  if (typeof a === "string") {
-    const timeStringFilter = z.string().datetime().safeParse(a);
+// Things Project
 
-    if (!timeStringFilter.success) {
-      const dateTimeTuple = z
-        .tuple([dateString, timeString])
-        .safeParse(a.split("@"));
-
-      if (!dateTimeTuple.success) {
-        dateTimeTuple.error.issues.forEach((iss) => ctx.addIssue(iss));
-        return z.NEVER;
-      }
-      return dateTimeTuple.data.join("@");
-    }
-    return format(parseISO(a), "MMMM do, y@h aaa");
-  }
-  return format(a, "MMMM do, y@h aaa");
+const projectAttributes = z.object({
+  title: z.string().optional(),
+  notes: z.string().optional(),
+  when,
+  deadline: dateString.optional(),
+  tags: z.string().array().optional(),
+  completed: z.boolean().optional(),
+  canceled: z.boolean().optional(),
+  "creation-date": dateString.optional(),
+  "completion-date": dateString.optional(),
+  "area-id": z.string().uuid().optional(),
+  area: z.string().optional(),
 });
 
-export type DateTimeString = z.output<typeof dateTimeString>;
-
-// Things Projects
-
-export const projectSchema = z.object({
-  type: z.literal("project"),
-  attributes: z
-    .object({
-      title: z.string().optional(),
-      notes: z.string().optional(),
-      when: z
-        .union([
-          z.enum(["today", "tomorrow", "evening", "anytime", "someday"]),
-          dateString,
-        ])
+export const projectSchema = z.union([
+  z.object({
+    type: z.literal("project"),
+    operation: z.literal("create"),
+    attributes: projectAttributes.extend({
+      items: todoSchema.array().nonempty().optional(),
+    }),
+  }),
+  z.object({
+    type: z.literal("project"),
+    operation: z.literal("update"),
+    attributes: projectAttributes.extend({
+      "prepend-notes": z.string().optional(),
+      "append-notes": z.string().optional(),
+      "add-tags": z
+        .string()
+        .array()
+        .nonempty()
+        .transform((a) => {
+          return a.join(",");
+        })
         .optional(),
-      deadline: dateString.optional(),
-      tags: z.string().array().optional(),
-      completed: z.boolean().optional(),
-      canceled: z.boolean().optional(),
-      "creation-date": dateString.optional(),
-      "completion-date": dateString.optional(),
-    })
-    .and(
-      z.union([
-        z.object({ "area-id": z.string().uuid().optional() }),
-        z.object({ area: z.string().optional() }),
-      ]),
-    ),
+    }),
+  }),
+]);
+
+export const thingsJsonSchema = z.object({
+  "auth-token": z.string().optional(),
+  reveal: z.boolean().optional(),
+  data: z.union([todoSchema, projectSchema]).array().nonempty(),
 });
-
-export type ProjectInput = z.input<typeof projectSchema>;
-
-export type ProjectOutput = z.output<typeof projectSchema>;
